63fe1f7a65c58e1d39a8657a051c71700ed83b3c
charlsjon <charlsjonangeloerrazo@gmail.com>
2024-04-30 17:48:17 +0800

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#F1F1F1',
+        secondary: '#2B2A2A',
+        tertiary: '#146758',
+        searchfill: '#BAB3B3',
+        searchplaceholder: '#5F6160',
+        grad: 'rgb(0 0 0 / 30%)',
+      },
+      size: {
+        '100': '100px',
+      },
+      fontSize: {
+        xxs: "10px"
+      }
+    }
+  }
+}
